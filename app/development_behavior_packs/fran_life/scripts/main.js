f93ca617aef445a6621913cdec738f49bf0aef51
Frank1o3 <jahdy1o3@gmail.com>
2025-03-14 17:31:16 +0000

    new file:   .gitignore 	deleted:    app/development_behavior_packs/fran_cc/manifest.json 	deleted:    app/development_behavior_packs/fran_cc/pack_icon.png 	modified:   app/development_behavior_packs/fran_life/scripts/main.js 	deleted:    app/development_resource_packs/fran_cc/manifest.json 	deleted:    app/development_resource_packs/fran_cc/pack_icon.png 	modified:   app/server.properties 	renamed:    app/worlds/Main/world_behavior_packs.json -> app/worlds/Main/world_behavior_pack.json 	renamed:    app/worlds/Main/world_resource_packs.json -> app/worlds/Main/world_resource_pack.json 	new file:   package-lock.json     new file:   package.json

--- app/development_behavior_packs/fran_life/scripts/main.js
+++ app/development_behavior_packs/fran_life/scripts/main.js
@@ -1,40 +1,64 @@
 // ct:/main.js
 import * as mc from "@minecraft/server";
+import * as net from "@minecraft/server-net"
+
 var HP_Objective = "HP";
 var Player_List = Array();
+
 function createObjective() {
-  let objective = mc.world.scoreboard.getObjectives().find((obj) => obj.id === HP_Objective);
-  if (!objective) {
-    objective = mc.world.scoreboard.addObjective(HP_Objective, HP_Objective);
-  }
-  mc.world.scoreboard.setObjectiveAtDisplaySlot(mc.DisplaySlotId.BelowName, { objective });
+	let objective = mc.world.scoreboard
+		.getObjectives()
+		.find((obj) => obj.id === HP_Objective);
+	if (!objective) {
+		objective = mc.world.scoreboard.addObjective(
+			HP_Objective,
+			HP_Objective
+		);
+	}
+	mc.world.scoreboard.setObjectiveAtDisplaySlot(mc.DisplaySlotId.BelowName, {
+		objective,
+	});
 }
+
 mc.world.afterEvents.playerSpawn.subscribe((eventData) => {
-  const Player = eventData.player;
-  const objective = mc.world.scoreboard.getObjectives().find((obj) => obj.id === HP_Objective);
-  if (!Player_List.find((plr) => plr === Player)) {
-    Player_List.push(Player);
-  }
-  if (objective && objective.isValid()) {
-    let health = Math.round(Player.getComponent("minecraft:health").currentValue);
-    objective.setScore(Player.name, health);
-  }
+	const Player = eventData.player;
+	const objective = mc.world.scoreboard
+		.getObjectives()
+		.find((obj) => obj.id === HP_Objective);
+	if (!Player_List.find((plr) => plr === Player)) {
+		Player_List.push(Player);
+	}
+	if (objective && objective.isValid()) {
+		let health = Math.round(
+			Player.getComponent("minecraft:health").currentValue
+		);
+		objective.setScore(Player.name, health);
+	}
 });
+
 mc.world.beforeEvents.playerLeave.subscribe((eventData) => {
-  const Player = eventData.player;
-  Player_List = Player_List.filter((plr) => plr !== Player);
+	const Player = eventData.player;
+	Player_List = Player_List.filter((plr) => plr !== Player);
 });
+
 function Main() {
-  const objective = mc.world.scoreboard.getObjectives().find((obj) => obj.id === HP_Objective);
-  Player_List.forEach((player) => {
-    if (objective && objective.isValid()) {
-      let health = Math.round(player.getComponent("minecraft:health").currentValue);
-      objective.setScore(player.name, health);
-    }
-  });
+	const objective = mc.world.scoreboard
+		.getObjectives()
+		.find((obj) => obj.id === HP_Objective);
+	Player_List.forEach((player) => {
+		if (objective && objective.isValid()) {
+			let health = Math.round(
+				player.getComponent("minecraft:health").currentValue
+			);
+			objective.setScore(player.name, health);
+		}
+	});
 }
+
 mc.system.run(() => {
-  console.log("Mod Running....");
-  createObjective();
+	console.log("Mod Running....");
+	createObjective();
+  
 });
+
 mc.system.runInterval(Main, 20);
